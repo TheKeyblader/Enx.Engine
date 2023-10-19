c4f97d7b412b362b8003b4216ed402a9f6708b7c
Jean-François Pustay <jef97310@gmail.com>
2023-10-20 00:02:57 +0200

    ....

---
+++ Sources/Core/Enx.Engine.Arch/Services/IEventManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Enx.Engine.Arch.Services;
+
+public interface IEventManager<T> : IEnumerable<T>
+{
+    public IEnumerable<T> NewerEvents { get; }
+    public IEnumerable<T> OldestEvents { get; }
+
+    void Send(T @event);
+    void SendDefault();
+    void SendBatch();
+
+
+    bool IsEmpty { get; }
+    int Count { get; }
+    void Clear();
+}
